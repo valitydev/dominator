934d616f0dfc5ab454bfb11d0e06c2e6a6a5cffa
ggmaleva <ggmaleva@yandex.ru>
2024-06-14 12:05:42 +0300

    add party modification additional info handler

--- src/test/java/com/empayre/dominator/TestData.java
+++ src/test/java/com/empayre/dominator/TestData.java
@@ -1,9 +1,15 @@
 package com.empayre.dominator;
 
 import dev.vality.damsel.domain.*;
+import dev.vality.damsel.payment_processing.*;
+import dev.vality.kafka.common.serialization.ThriftSerializer;
+import dev.vality.machinegun.eventsink.MachineEvent;
+import dev.vality.machinegun.msgpack.Value;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -25,5 +31,39 @@ public class TestData {
 
     public static String randomString() {
         return UUID.randomUUID().toString();
+    }
+
+    public static PartyChange createPartyChangeWithPartyModificationAdditionalInfo(String name,
+                                                                                   String comment,
+                                                                                   String... emails) {
+        AdditionalInfoModificationUnit additionalInfoModificationUnit = new AdditionalInfoModificationUnit();
+        additionalInfoModificationUnit.setPartyName(name);
+        additionalInfoModificationUnit.setComment(comment);
+        additionalInfoModificationUnit.setManagerContactEmails(Arrays.stream(emails).toList());
+        PartyModification partyModification = new PartyModification();
+        partyModification.setAdditionalInfoModification(additionalInfoModificationUnit);
+        Claim claim = createClaim();
+        claim.setChangeset(List.of(partyModification));
+        PartyChange partyChange = new PartyChange();
+        partyChange.setClaimCreated(claim);
+        return partyChange;
+    }
+
+    public static Claim createClaim() {
+        Claim claim = new Claim();
+        claim.setId(1);
+        claim.setCreatedAt("2023-07-03T10:15:30Z");
+        claim.setStatus(ClaimStatus.accepted(new ClaimAccepted()));
+        claim.setRevision(1);
+        return claim;
+    }
+
+    public static MachineEvent createPartyEventDataMachineEvent(PartyEventData partyEventData, String id) {
+        return new MachineEvent()
+                .setEventId(2L)
+                .setSourceId(id)
+                .setSourceNs("2")
+                .setCreatedAt("2021-05-31T06:12:27Z")
+                .setData(Value.bin(new ThriftSerializer<>().serialize("", partyEventData)));
     }
 }
---
+++ src/test/java/com/empayre/dominator/handler/event/impl/partymngmnt/party/PartyModificationAdditionalInfoHandlerTest.java
@@ -0,0 +1,54 @@
+package com.empayre.dominator.handler.event.impl.partymngmnt.party;
+
+import com.empayre.dominator.TestData;
+import com.empayre.dominator.config.PostgresqlSpringBootITest;
+import com.empayre.dominator.dao.party.iface.PartyDao;
+import com.empayre.dominator.domain.tables.pojos.Party;
+import com.empayre.dominator.handler.event.stock.impl.partymngmnt.party.PartyModificationAdditionalInfoHandler;
+import dev.vality.damsel.payment_processing.PartyChange;
+import dev.vality.damsel.payment_processing.PartyEventData;
+import dev.vality.machinegun.eventsink.MachineEvent;
+import dev.vality.testcontainers.annotations.util.RandomBeans;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@PostgresqlSpringBootITest
+class PartyModificationAdditionalInfoHandlerTest {
+
+    @Autowired
+    PartyModificationAdditionalInfoHandler handler;
+
+    @Autowired
+    PartyDao partyDao;
+
+    @Test
+    void handle() {
+        String partyName = "partyName";
+        String comment = "comment";
+        String email = "test@mail";
+        Party party = RandomBeans.random(Party.class);
+        party.setCurrent(Boolean.TRUE);
+        partyDao.save(party);
+        PartyChange partyChange =
+                TestData.createPartyChangeWithPartyModificationAdditionalInfo(partyName, comment, email);
+        PartyEventData partyEventData = new PartyEventData();
+        partyEventData.setChanges(List.of(partyChange));
+        MachineEvent machineEvent = TestData.createPartyEventDataMachineEvent(partyEventData, party.getPartyId());
+
+        handler.handle(partyChange, machineEvent, 1);
+
+        Party partyResult = partyDao.get(party.getPartyId());
+        assertNotNull(partyResult);
+        assertEquals(partyName, partyResult.getName());
+        assertEquals(comment, partyResult.getComment());
+        assertThat(partyResult.getManagerContactEmails(), containsString(email));
+    }
+}
