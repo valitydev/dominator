a4efc6df6bc2a34bddc868b01b4b8fd960f3cf70
malkoas <alien.life.form.1986@gmail.com>
2025-04-08 14:51:16 +0300

    BG-55: Bump flyway

--- src/test/java/dev/vality/dominator/service/PartyManagementServiceTest.java
+++ src/test/java/dev/vality/dominator/service/PartyManagementServiceTest.java
@@ -16,7 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -37,7 +37,7 @@ class PartyManagementServiceTest {
     @Autowired
     PartyManagementService partyManagementService;
 
-    @MockBean
+    @MockitoBean
     PartyDao partyDao;
 
     @BeforeEach
